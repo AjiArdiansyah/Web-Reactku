f78580c466c91568ccb76ba516eea846294fbfc9
AjiArdiansyah <55706264+AjiArdiansyah@users.noreply.github.com>
2022-03-12 01:24:46 +0700

    prak 4

---
+++ src/component/HelloComponent.js
@@ -0,0 +1,11 @@
+// Agar function arrow HelloComponent bisa berjalan di react maka tambahan react 
+
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+    return <p className='text-p'> Nama saya adalah Aji</p>
+}
+
+// agar component ini dapat dipakai dimana saja
+export default HelloComponent;
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+import BlogPost from './container/BlogPost/BlogPost';
+
+
+// function HelloComponent(){
+//   return <p>HelloComponent</p>
+// }
+
+// class StateFullComponent extends React.Component {
+//   render() {
+//     return <p>Statefull Component</p>;
+//   }
+// }
+
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+
+
+ReactDOM.render(<BlogPost/>, document.getElementById('content'))
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
